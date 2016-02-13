d224790ba2e7a4b7eced8b689702379f9f3c5a72
Paulo Sousa <prsousa34@gmail.com>
2016-02-13 02:22:22 +0000

    Added some Models (Java Classes)

---
+++ app/src/main/java/tk/geohealth/geohealth/models/SolarCream.java
@@ -0,0 +1,7 @@
+package tk.geohealth.geohealth.models;
+
+/**
+ * Created by Paulo on 13/02/16.
+ */
+public class SolarCream {
+}
---
+++ app/src/main/java/tk/geohealth/geohealth/models/SolarExposition.java
@@ -0,0 +1,12 @@
+package tk.geohealth.geohealth.models;
+
+import java.util.GregorianCalendar;
+
+/**
+ * Created by GeoHealth on 13/02/16.
+ */
+public class SolarExposition {
+    private GregorianCalendar inicio;
+    private boolean acabou;
+//    private
+}
